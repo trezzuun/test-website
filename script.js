395ea42c688b1b02069bcf37ed4f59f7bdf0e3a8
Trezzuun <66029983+trezzuun@users.noreply.github.com>
2023-12-15 05:21:20 -0500

    added christmas countdown

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("viewCount").textContent = viewCount;
 });
 
+var countDownDate = new Date("Dec 25, 2023 00:00:00").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                
+    document.getElementById("days").innerHTML = days;
+                
+          document.getElementById("hrs").innerHTML = hours;
+                
+                document.getElementById("mins").innerHTML = minutes;
+                
+                document.getElementById("secs").innerHTML = seconds;
+}, 1000);
+
 const elts = {
     text1: document.getElementById("text1"),
     text2: document.getElementById("text2")
