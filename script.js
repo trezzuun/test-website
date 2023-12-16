f0a0e6b568d5ce7e3747b872c1db980f831838ff
Trezzuun <66029983+trezzuun@users.noreply.github.com>
2023-12-16 09:04:14 -0500

    typewriter effect on bio

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
 // CHRISTMAS COUNTDOWN
 var countDownDate = new Date("Dec 25, 2023 00:00:00").getTime();
 
-var x = setInterval(function() {
+var x = setInterval(function () {
   var now = new Date().getTime();
   var distance = countDownDate - now;
-  
+
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  
+
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  
+
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  
+
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                
-    document.getElementById("days").innerHTML = days;
-                
-          document.getElementById("hrs").innerHTML = hours;
-                
-                document.getElementById("mins").innerHTML = minutes;
-                
-                document.getElementById("secs").innerHTML = seconds;
+
+  document.getElementById("days").innerHTML = days;
+
+  document.getElementById("hrs").innerHTML = hours;
+
+  document.getElementById("mins").innerHTML = minutes;
+
+  document.getElementById("secs").innerHTML = seconds;
 }, 1000);
 // END OF CHRISTMAS COUNTDOWN
 
 // FUSING TEXT EFFECT
 const elts = {
-    text1: document.getElementById("text1"),
-    text2: document.getElementById("text2")
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2"),
 };
 
-const texts = [
-    "CLICK",
-    "ANYWHERE",
-    "TO",
-    "ENTER"
-];
+const texts = ["CLICK", "ANYWHERE", "TO", "ENTER"];
 
 const morphTime = 1;
 const cooldownTime = 0.25;
@@ -48,60 +43,60 @@ elts.text1.textContent = texts[textIndex % texts.length];
 elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 
 function doMorph() {
-    morph -= cooldown;
-    cooldown = 0;
+  morph -= cooldown;
+  cooldown = 0;
 
-    let fraction = morph / morphTime;
+  let fraction = morph / morphTime;
 
-    if (fraction > 1) {
-        cooldown = cooldownTime;
-        fraction = 1;
-    }
+  if (fraction > 1) {
+    cooldown = cooldownTime;
+    fraction = 1;
+  }
 
-    setMorph(fraction);
+  setMorph(fraction);
 }
 
 function setMorph(fraction) {
-    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
-    fraction = 1 - fraction;
-    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
 
-    elts.text1.textContent = texts[textIndex % texts.length];
-    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 }
 
 function doCooldown() {
-    morph = 0;
+  morph = 0;
 
-    elts.text2.style.filter = "";
-    elts.text2.style.opacity = "100%";
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
 
-    elts.text1.style.filter = "";
-    elts.text1.style.opacity = "0%";
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
 }
 
 function animate() {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
-    let newTime = new Date();
-    let shouldIncrementIndex = cooldown > 0;
-    let dt = (newTime - time) / 1000;
-    time = newTime;
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 1000;
+  time = newTime;
 
-    cooldown -= dt;
+  cooldown -= dt;
 
-    if (cooldown <= 0) {
-        if (shouldIncrementIndex) {
-            textIndex++;
-        }
-
-        doMorph();
-    } else {
-        doCooldown();
+  if (cooldown <= 0) {
+    if (shouldIncrementIndex) {
+      textIndex++;
     }
+
+    doMorph();
+  } else {
+    doCooldown();
+  }
 }
 
 animate();
@@ -109,12 +104,12 @@ animate();
 
 // Function to handle click event and hide the welcome screen
 function handleClick() {
-    document.getElementById('welcomeScreen').style.display = 'none'; // Hide the welcome screen
-    document.getElementById('websiteContent').style.display = 'block'; // Show the website content
-  }
+  document.getElementById("welcomeScreen").style.display = "none"; // Hide the welcome screen
+  document.getElementById("websiteContent").style.display = "block"; // Show the website content
+}
 
-  // Attach click event listener to the whole document
-  document.addEventListener('click', handleClick);
+// Attach click event listener to the whole document
+document.addEventListener("click", handleClick);
 
 // STOP PEOPLE FROM STEALING
 document.addEventListener("contextmenu", function (e) {
@@ -137,7 +132,6 @@ document.onkeydown = function (e) {
 };
 // END OF WELCOME SCREEN
 
-
 // WEBSITE COUNTER
 var counterContainer = document.querySelector(".view-counter");
 var visitCount = localStorage.getItem("page_view");
@@ -151,5 +145,65 @@ if (visitCount) {
   localStorage.setItem("page_view", 1);
 }
 counterContainer.innerHTML = visitCount;
-
 // END OF WEBSITE COUNTER
+
+// START OF TYPEWRITER EFFECT BIO
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+// END OF TYPEWRITER EFFECT BIO
