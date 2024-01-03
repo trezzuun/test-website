a029f1a17e146a29def2f938fc92ff8381b5a050
Trezzuun <66029983+trezzuun@users.noreply.github.com>
2024-01-03 16:22:25 -0500

    updated website

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // CHRISTMAS COUNTDOWN
-var countDownDate = new Date("Dec 25, 2023 00:00:00").getTime();
+//var countDownDate = new Date("Dec 25, 2023 00:00:00").getTime();
 
-var x = setInterval(function () {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
+//var x = setInterval(function () {
+  //var now = new Date().getTime();
+  //var distance = countDownDate - now;
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  //var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("days").innerHTML = days;
+  //document.getElementById("days").innerHTML = days;
 
-  document.getElementById("hrs").innerHTML = hours;
+  //document.getElementById("hrs").innerHTML = hours;
 
-  document.getElementById("mins").innerHTML = minutes;
+  //document.getElementById("mins").innerHTML = minutes;
 
-  document.getElementById("secs").innerHTML = seconds;
-}, 1000);
+  //document.getElementById("secs").innerHTML = seconds;
+//}, 1000);
 // END OF CHRISTMAS COUNTDOWN
 
 // FUSING TEXT EFFECT
